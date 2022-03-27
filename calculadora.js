af40f95a2c503227fe9817fd95cfb634ec7b77fd
EduardoJosueGodoy <edudesm38@gmail.com>
2022-03-27 13:26:37 -0300

    [200~

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+let sumar = require ('./sumar');
+let restar = require ('./restar');
+let multiplicar= require ('./multiplicar');
+let dividir = require ('./dividir');
+console.log(sumar(1,2))
+console.log(restar(1,2))
+console.log(multiplicar(1,2))
+console.log(multiplicar(1,0))
+console.log(dividir(10,5))
+console.log(dividir(0,2))
